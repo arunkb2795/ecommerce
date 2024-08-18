41233e55a0150c571aa5ad142d49a57a49e0677f
arunkb2795 <arunkb27@gmail.com>
2024-08-18 12:46:19 +0530

    Update : header nav links

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       {/* BIGGER SCREENS */}
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
         {/* LEFT */}
-        <div className="w-1/3">
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
           <Link href="#" className="flex items-center gap-3">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -35,9 +35,16 @@ const Navbar = () => {
 
             <div className="text-2xl tracking-wide">Ekart</div>
           </Link>
+          <div className="hidden xl:flex gap-4">
+            <Link href="/">Home</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
         </div>
         {/* RIGHT */}
-        <div className="w-2/3 flex justify-between gap-8 items-center">
+        <div className="w-2/3 xl:w-1/2 flex justify-between gap-8 items-center">
           <SearchBar />
           <NavIcons />
         </div>
--- src/components/nav-icons.tsx
+++ src/components/nav-icons.tsx
@@ -1,40 +1,73 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 const NavIcons = () => {
+  const [isProfileOpen, setIsProfileOpen] = useState<boolean>(false);
+  const [isCartOpen, setIsCartOpen] = useState<boolean>(false);
+  const router = useRouter();
+  // TEMPORARY
+  const isLoggedIn = false;
+
+  const handleProfile = () => {
+    if (!isLoggedIn) {
+      setIsProfileOpen((prev) => !prev);
+    }
+    router.push("/login");
+  };
+
   return (
-    <div className="flex items-center gap-4 2xl:gap-6">
-        <div className="flex gap-2">
+    <div className="flex items-center gap-4 2xl:gap-6 relative">
+      <div className="flex gap-2 cursor-pointer" onClick={handleProfile}>
         <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-        />
-      </svg>
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+        </svg>
+        Joe John
+      </div>
 
-      Joe John
-
+      {isProfileOpen && (
+        <div className="absolute p-4 rounded-md top-12 left-0 right-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+          <Link href="/">Profile</Link>
+          <div className="mt-2 cursor-pointer">Logout</div>
         </div>
+      )}
 
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-        />
-      </svg>
+      <div className="cursor-pointer relative">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+          onClick={() => setIsCartOpen((prev) => !prev)}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+          />
+        </svg>
+        <div className="absolute -top-4 -right-4 w-6 h-6 bg-pink_red rounded-full text-white text-sm flex justify-center items-center">4</div>
+      </div>
+      {isCartOpen && (
+          <div className="absolute p-4 rounded-md top-12 left-0 right-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+            Cart Modal
+          </div>
+        )}
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
