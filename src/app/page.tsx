c5d4c1e2c0566fe8fd6f214da8223fa7424d286b
arunkb2795 <arunkb27@gmail.com>
2024-08-21 21:42:43 +0530

    Update : slider component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Slider from "@/components/slider";
 import { WixClientServer } from "@/lib/wixClientServer";
 
 const HomePage = async () => {
   const wixClient = await WixClientServer();
   const res = await wixClient.products.queryProducts().find();
   console.log({res})
-  return <div className="">Home Page</div>;
+  return <div className="">
+    <Slider/>
+  </div>;
 };
 
 export default HomePage;
